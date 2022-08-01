cbbd59940aaae021338b18e19ddc63eef2bac0d9
wendikardian <wendikardian@gmail.com>
2022-08-01 13:31:09 +0700

    :)

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles'
+
+const App = () => {
+    const classes = useStyles()
+    return(
+        <Container maxwidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit" >
+                <Typography variant="h2" align="center" className={classes.heading}>Memories</Typography>
+                <img src={memories} alt="memories" height="60" className={classes.image} />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import postRoutes from './routes/posts.js'
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({limit : "30mb", extended : true}))
+app.use(bodyParser.urlencoded({limit : "30mb", extended : true}))
+app.use(cors())
+
+// mongo db
+
+const CONNECTION_URL = 'mongodb+srv://wendikardian:qwerty12345@wendicluster.k2zun.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)) )
+.catch((error) => console.error(error.message) )
+
+// mongoose.set('useFindAndModify', false)
+// await mongoose.connect('mongodb://localhost:27017/test', {
+//   useNewUrlParser: true, // <-- no longer necessary
+//   useUnifiedTopology: true // <-- no longer necessary
+// });
